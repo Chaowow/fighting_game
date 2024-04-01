3735a16eb5dd640d692db189c3c5f0869db36d9e
Frankie <frankiejosefej@gmail.com>
2024-04-01 18:22:44 -0400

    Implemented health bar interface

--- index.js
+++ index.js
@@ -27,6 +27,7 @@ class Sprite {
         };
         this.color = color;
         this.isAttacking;
+        this.health = 100;
     }
 
     draw() {
@@ -159,7 +160,8 @@ function animate() {
         player.isAttacking
     ) {
         player.isAttacking = false;
-        console.log('Collision!');
+        enemy.health -= 10;
+        document.querySelector('#enemyHealth').style.width = enemy.health + '%';
     }
 
     if (
@@ -170,7 +172,8 @@ function animate() {
             enemy.isAttacking
         ) {
             enemy.isAttacking = false;
-            console.log('GO!');
+            player.health -= 10;
+            document.querySelector('#playerHealth').style.width = player.health + '%';
         }
 }
 
